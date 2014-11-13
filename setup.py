fb657f96449242bd9dab4d8a437d457f87d93e8c
Geoff Hing <geoffhing@gmail.com>
2014-11-13 11:05:09 -0600

    Update dependencies

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
+        'requests',
+        'lxml',
+        'cssselect',
     ],
     tests_require=[
         'nose',
