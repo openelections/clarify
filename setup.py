291f6824447d460a26a5cf6f6b06d71c01ee78a0
Geoff Hing <geoffhing@gmail.com>
2014-11-13 10:02:31 -0600

    Bootstrap package
    Scaffolding for tests, setup.py, empty package.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='OpenElections Clarity',
+    version='0.1.0',
+    author='OpenElections',
+    author_email='openelections@gmail.com',
+    url='http://openelections.net',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+    tests_require=[
+        'nose',
+    ],
+)
